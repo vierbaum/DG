d158949c49086a0c8104a3da1678a04218d52994
nld <nld@gen.fritz.box>
2021-12-02 22:07:46 +0100

    bug fixes

--- main.py
+++ main.py
@@ -21,14 +21,13 @@ def dec_num(num):
         return vars[1][vars[0].index(num)]
 
 def write_to_var(params):
-        if params[1] in vars[0]:
-            vars[1][vars[0].index(params[1])] = dec_num(params[2])
-        else:
-            vars[0].append(params[1])
-            vars[1].append(float(params[2]))
+    if params[1] in vars[0]:
+        vars[1][vars[0].index(params[1])] = dec_num(params[2])
+    else:
+        vars[0].append(params[1])
+        vars[1].append(float(params[2]))
 
 def operate(params):
-    # calculate
     if params[0] == "+":
         print(dec_num(params[1]) + dec_num(params[2]))
 
@@ -44,7 +43,6 @@ def operate(params):
     if params[0] == "%":
         print(dec_num(params[1]) % dec_num(params[2]))
 
-    # vars
     if params[0] == "v":
         if params[2] == "o":
             if params[3] == "+":
@@ -62,6 +60,9 @@ def operate(params):
             if params[3] == "%":
                 write_to_var([0, params[1], (dec_num(params[4]) % dec_num(params[5]))])
 
+            if params[3] == "^":
+                write_to_var([0, params[1], (dec_num(params[4]) ** dec_num(params[5]))])
+
         else:
             write_to_var(params)
 
@@ -76,13 +77,12 @@ def operate(params):
         var = new_code[0][1].split(",")
 
         for i in range(int(var[0]), int(var[1]), int(var[2])):
-            for l in range(len(new_code)):
-                if new_code[l][0] == "l":
-                    write_to_var([0, new_code[l][2], i])
-            for l in range(len(new_code)):
-                operate(new_code[l][(3 - len(new_code[l])):])
+            write_to_var([0, new_code[0][2], i])
+            for l in range(len(new_code) - 1):
+                operate(new_code[l + 1])
 
-    # logical
+
+
     if params[0] == "<":
         if dec_num(params[1]) < dec_num(params[2]):
             operate(params[(3 - len(params)):])
@@ -100,7 +100,7 @@ def operate(params):
             operate(params[(3 - len(params)):])
 
 for line in Lines:
-    if "&" in line.strip():
+    if line.strip()[0] == "l":
 
         operate(line.strip())
         try:
@@ -113,4 +113,3 @@ for line in Lines:
             operate(code)
         except:
             IndexError
-

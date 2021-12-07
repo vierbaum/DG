5002167048ed3e9f50e1da272cd7cdb2a14b2e1b
nld <nld@gen.fritz.box>
2021-12-07 17:33:10 +0100

    some changes

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def write_to_var(params):
     if params[2] == "[]":
         vars[0].append(params[1])
         vars[1].append([])
-
     elif params[1] in vars[0]: vars[1][vars[0].index(params[1])] = dec_num(params[2])
     else:
         vars[0].append(params[1])
@@ -35,10 +34,9 @@ def operate(params):
             write_to_var(params)
 
     if params[0] == "l":
-        new_code = [params[0:params.index("&")].split(" "), params[params.index("&") + 1:]]
-        for i in range(int(dec_num(new_code[0][1].split(",")[0])), int(dec_num(new_code[0][1].split(",")[1])), int(dec_num(new_code[0][1].split(",")[2]))):
-            write_to_var([0, new_code[0][2], i])
-            operate(new_code[1]) if new_code[1][0] == "l" else [operate([i for i in re.split(r' |(?<=")(.*)(?=")', j) if i != "\"" and i != None]) for j in new_code[1].split("&")]
+        for i in range(int(dec_num([params[0:params.index("&")].split(" "), params[params.index("&") + 1:]][0][1].split(",")[0])), int(dec_num([params[0:params.index("&")].split(" "), params[params.index("&") + 1:]][0][1].split(",")[1])), int(dec_num([params[0:params.index("&")].split(" "), params[params.index("&") + 1:]][0][1].split(",")[2]))):
+            write_to_var([0, [params[0:params.index("&")].split(" "), params[params.index("&") + 1:]][0][2], i])
+            operate([params[0:params.index("&")].split(" "), params[params.index("&") + 1:]][1]) if [params[0:params.index("&")].split(" "), params[params.index("&") + 1:]][1][0] == "l" else [operate([i for i in re.split(r' |(?<=")(.*)(?=")', j) if i != "\"" and i != None]) for j in [params[0:params.index("&")].split(" "), params[params.index("&") + 1:]][1].split("&")]
 
     if params[0] == "<" and dec_num(params[1]) < dec_num(params[2]): operate(params[(3 - len(params)):])
     if params[0] == ">" and dec_num(params[1]) > dec_num(params[2]): operate(params[(3 - len(params)):])
